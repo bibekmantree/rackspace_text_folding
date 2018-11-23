ea915a0a3327248ee3a617d3e58a62bb0706c840
bibekmantree <badbond420@gmail.com>
2018-11-23 05:14:41 +0000

    windows is not that bad, I guess

--- script.py
+++ script.py
@@ -1,4 +1,29 @@
 import fileinput
 
+def get_number(flag, number_of_astr):
+    position = number_of_astr - 1
+    try:
+        flag[position] += 1
+        flag = flag[:position+1]
+    except:
+        flag.append(1)
+    return flag
+
+def my_print(chunk, flag):
+	line = chunk[0]
+	number_of_astr = len(line) - len(line.lstrip("*"))
+	flag = get_number(flag, number_of_astr)
+	print(".".join([str(i) for i in flag]), line.lstrip("*"), end="")
+	
+
+chunk = []
+flag = [0]
 for line in fileinput.input():
-        print("*",line)
+	if line.startswith("*"):
+		my_print(chunk, flag)
+		chunk = []
+		chunk.append(line)
+	else:
+		chunk.append(line)
+	
+		
